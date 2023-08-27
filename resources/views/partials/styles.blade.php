a33fa12db1a5ec3b14f23d7bf5b8ab5bac6db6e3
single-byte <70197335+single-byte@users.noreply.github.com>
2023-08-27 10:12:24 +0500

    updated styles

--- resources/views/partials/styles.blade.php
+++ resources/views/partials/styles.blade.php
@@ -2,28 +2,16 @@
 <script src="https://cdn.tailwindcss.com"></script>
 
 <style>
-* {
-  font-family: 'Poppins', sans-serif;
-}
+  * {
+    font-family: 'Poppins', sans-serif;
+  }
 
+  .tab-content {
+    display: none;
+  }
 
-/* Tab Content */
-.tab-content {
-  display: none;
-}
-
-/* Active Tab Content */
-.active {
-  display: block;
-}
-
-/* Tab Buttons */
-.tab-button {
-  /* cursor: pointer;
-  padding: 10px 20px;
-  background-color: #f0f0f0;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  margin-right: 10px; */
-}
+  /* Active Tab Content */
+  .active {
+    display: block;
+  }
 </style>
